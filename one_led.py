55242fe79cdcbad22ab85c3dd8c0fa4c1514fd38
Andrew Kleindolph <akleindolph@gmail.com>
2021-01-22 11:36:39 -0800

    Update one_led.py

--- one_led.py
+++ one_led.py
@@ -5,8 +5,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)   # sets pin 18 as output
 
-var = 1
-while var == 1 :  # This constructs an infinite loop
+while True :  # This constructs an infinite loop
     print ("LED on")
     GPIO.output(18,GPIO.HIGH)
     time.sleep(2.5)   #two and half second delay
