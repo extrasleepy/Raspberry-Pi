52d41794227b15738fafcd165230c9d3d5325f7b
Andrew Kleindolph <akleindolph@gmail.com>
2021-01-22 12:17:48 -0800

    Delete nyt_most_viewed.py

--- nyt_most_viewed.py
+++
@@ -1,9 +0,0 @@
-from requests import get
-import json
-
-url='https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=your_key_here'
-nyt = get(url).json()
-data = json.dumps(nyt, sort_keys=True, indent=4)
-
-for x in range(0, 5):
-    print(nyt['results'][x]['title'])
