7436c8c99a03cca0cb5cb25807f8b0b20171c5ef
Andrew Kleindolph <akleindolph@gmail.com>
2021-02-04 18:52:16 -0800

    Update schedule_test.py

--- schedule_test.py
+++ schedule_test.py
@@ -1,1 +1,13 @@
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)   # sets pin 18 as output
+
+print ("LED on")
+GPIO.output(18,GPIO.HIGH)
+time.sleep(2.5)   #two and half second delay
+print ("LED off")
+GPIO.output(18,GPIO.LOW)
+time.sleep(2)
