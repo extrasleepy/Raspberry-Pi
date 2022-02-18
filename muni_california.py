5cf26694c5c2a38b951016d494b1152efcbb5026
Andrew Kleindolph <akleindolph@gmail.com>
2022-02-18 10:52:52 -0800

    Update muni_california.py

--- muni_california.py
+++ muni_california.py
@@ -2,7 +2,7 @@ import time
 from requests import get
 import json
 
-url='http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&stopId=13859&routetag=1-california'
+url='https://retro.umoiq.com/service/publicJSONFeed?command=predictions&a=sf-muni&stopId=13859&routetag=1-california'
 muni = get(url).json()['predictions']
 data = json.dumps(muni, sort_keys=True, indent=4)
 #print(data)
