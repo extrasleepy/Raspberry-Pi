93167b8bf1f4e6c2275634a0170cd928e507e721
Andrew Kleindolph <akleindolph@gmail.com>
2023-01-26 15:05:27 -0800

    Update muni_california.py

--- muni_california.py
+++ muni_california.py
@@ -9,6 +9,5 @@ while True:
    muni_route = get(url).json()[0]['route']['title']
    muni_stop = get(url).json()[0]['stop']['name']
    muni_eta = get(url).json()[0]['values'][0]['minutes']
-   #print(muni['direction']['prediction'][0]['minutes'])
    print(muni_route + " arrives in " + str(muni_eta) + " minutes at " + str(muni_stop) + " stop")
    time.sleep(30)
