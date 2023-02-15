9b162136c497e6732802ccb05667b062d43241c8
Andrew Kleindolph <akleindolph@gmail.com>
2023-02-14 16:00:14 -0800

    Update pi_28_screen_test.py

--- pi_28_screen_test.py
+++ pi_28_screen_test.py
@@ -14,8 +14,8 @@ blue = (0, 0, 128)
 black = (0, 0, 0)
  
 # assigning values to X and Y variable
-X = 1280
-Y = 900
+X = 1080
+Y = 720
 
 # create the display surface object
 # of specific dimension..e(X, Y).
