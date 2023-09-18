dcfbc38ea545d4bca9a10c815009b1332b3b153a
Andrew Kleindolph <akleindolph@gmail.com>
2023-09-18 10:08:50 -0700

    Update button.py

--- button.py
+++ button.py
@@ -2,11 +2,15 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 print('start')
-while True:
-    input_state = GPIO.input(19)
-    if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.2)
+
+try:
+    while True:
+        input_state = GPIO.input(19)
+        if input_state == False:
+            print('Button Pressed')
+            time.sleep(0.2)
+
+except KeyboardInterrupt:  #end program if control+c pressed
+    GPIO.cleanup()   #reset all the pins you've used
