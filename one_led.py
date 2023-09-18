013a82699303ed90497950a47496d9e2f8f3457b
Andrew Kleindolph <akleindolph@gmail.com>
2023-09-18 09:46:18 -0700

    Update one_led.py

--- one_led.py
+++ one_led.py
@@ -5,10 +5,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)   # sets pin 18 as output
 
-while True :  # This constructs an infinite loop
-    print ("LED on")
-    GPIO.output(18,GPIO.HIGH)
-    time.sleep(2.5)   #two and half second delay
-    print ("LED off")
-    GPIO.output(18,GPIO.LOW)
-    time.sleep(2)
+try:
+    while True :  # This constructs an infinite loop
+        print ("LED on")
+        GPIO.output(18,GPIO.HIGH)
+        time.sleep(2.5)   #two and half second delay
+        print ("LED off")
+        GPIO.output(18,GPIO.LOW)
+        time.sleep(2)
+
+except KeyboardInterrupt:  #end program if control+c pressed
+    pass
+finally:
+    GPIO.cleanup()   #reset all the pins you've used
